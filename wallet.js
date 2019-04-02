7c895f435bd0798ff076620b36ea5411c5bd195e
lukaszinglot <luk.inglot@gmail.com>
2019-04-02 04:37:13 +0200

    add js file which is responsible for player credits, its also check player wallet value and add or subtract wallet money

---
+++ wallet.js
@@ -0,0 +1,30 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+        //actual value of the wallet
+        this.getWalletValue = () => _money;
+        //check players wallet value
+        this.checkCanPlay = value => {
+            if (_money >= value) return true;
+            return false;
+        }
+        //Add or subtract value in wallet
+        this.changeWallet = (value, type = "+") => {
+            if (typeof value === "number" && !isNaN(value)) {
+                if (type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("nieprawidlowy typ dzialania")
+                }
+            } else {
+                console.log(typeof value);
+                throw new Error("nieprawidlowa liczba")
+            }
+        }
+    }
+}
+
+
+const wallet = new Wallet(200)
